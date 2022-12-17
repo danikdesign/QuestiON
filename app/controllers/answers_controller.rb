41ae7ef320ab3e594e28058091d47beb1da88bae
danikdesign <danik.newmail@gmail.com>
2022-12-17 17:16:58 +0200

    Added CRUD for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :set_question!
+  before_action :set_question!, :set_answer!
 
   def create
     @answer = @question.answers.build answer_params
@@ -8,13 +8,40 @@ class AnswersController < ApplicationController
       flash[:success] = "Answer created!"
       redirect_to question_path(@question)
     else
+      @answers = Answer.order created_at: :desc
       render 'questions/show'
     end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @answer.update answer_params
+      redirect_to question_path(@question)
+    else
+      @answers = Answer.order created_at: :desc
+      render :edit
+    end
+
+  end
+
+  def destroy
+    answer = @question.answers.find params[:id]
+    answer.destroy
+    flash[:success] = "Answer deleted!"
+
+    redirect_to question_path(@question)
   end
 
   private
   def set_question!
     @question = Question.find params[:question_id]
+  end
+
+  def set_answer!
+    @answer = @question.answers.find params[:id]
   end
   def answer_params
     params.require(:answer).permit(:body)
